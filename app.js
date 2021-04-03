8ebfc09eb22f87631cbd4f750c3eca819c18a97a
mrbaig97 <77655571+mrbaig97@users.noreply.github.com>
2021-04-03 19:40:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// ARRAYS AND LOOP (Chap 17-30)
+// 1. Declare and initialize an empty multidimensional array. (Array of arrays)
+// let activities = [
+//     ['Work', 9],
+//     ['Eat', 1],
+//     ['Commute', 2],
+//     ['Play Game', 1],
+//     ['Sleep', 7]
+// ];
+// 2. Declare and initialize a multidimensional array representing the following matrix:
+//var dimension = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+//document.write(dimension);
+// 3. Write a program to print numeric counting from 1 to 10.
+// var counting;
+// for(counting=1;counting<11;counting++){
+//     document.write(counting+"<br/>");
+// }
+//4. Write a program to print multiplication table of any number using for loop.
+// Table number & length should be taken as an input from user.
+// var table = +prompt("Enter no for table?");
+// var lengthOftable = +prompt("Enter table length?");
+// for (i=1;i<=lengthOftable;i++){
+//     document.write(table + " x  " +  i+" = " + table*i+"<br/>" );
+// }
